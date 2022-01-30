e2c715f6dab01fb53b16487492afb12faf0e2cd4
ElectRocnic <electrocnicofficial@gmail.com>
2022-01-30 12:35:16 +0100

    Change collision detection for bullets: Introduce new physics layers to be able to collide bullets agains walls but not collide but still trigger death on player.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -36,7 +36,7 @@ public class Bullet : MonoBehaviour
         this.origin = origin;
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject == origin)
         {
