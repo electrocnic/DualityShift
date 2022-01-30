d5d05133a29cae6c178b842e0f0da58d3297e579
ElectRocnic <electrocnicofficial@gmail.com>
2022-01-30 15:02:53 +0100

    Add LevelController script and gem which triggers next level on touch

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour {
+    [SerializeField] private string nextLevelName;
+    // [SerializeField] private Transform levelExitTarget;
+    private GameObject player;
+    
+    private void OnEnable() {
+        //m_Monsters = FindObjectsOfType<>()
+    }
+
+    void Start() {
+        player = Resources.FindObjectsOfTypeAll<CharacterController2d>()[0].gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: idea: if all monsters dead: win
+        //if (reachedLevelExit()) {
+        //    goToNextLevel();
+        //}
+    }
+
+    private void goToNextLevel() {
+        SceneManager.LoadScene(nextLevelName);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (!player) {
+            return;
+        }
+
+        if (player == col.gameObject) {
+            goToNextLevel();
+        }
+    }
+}
