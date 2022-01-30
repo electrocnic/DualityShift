b2c2992b59cbfb67a135a9ea4c8692e978a38b11
ambiso <ambiso@invalid>
2022-01-30 10:08:04 +0100

    Fix potion collision

--- Assets/Scripts/PotionPickup.cs
+++ Assets/Scripts/PotionPickup.cs
@@ -7,8 +7,19 @@ public class PotionPickup : MonoBehaviour
 {
     [SerializeReference] private CharacterController2d m_CharacterController2d;
     [SerializeField] private float m_ValuePerAddedPotion = 0.175f;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = Resources.FindObjectsOfTypeAll<CharacterController2d>()[0].gameObject;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject != player)
+        {
+            return;
+        }
         Destroy(gameObject);
         m_CharacterController2d.setPotionFillStatus(MathF.Min(1.0f,m_CharacterController2d.getPotionFillStatus() + m_ValuePerAddedPotion));
     }
